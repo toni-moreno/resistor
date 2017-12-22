589ff4f7867179aaa69b538c18e0483b5a56b94f
sbengo <serigna@gmail.com>
2017-12-22 12:41:14 +0100

    UI Fixed capitalized URL in HTTPOut component

--- src/app/outhttp/outhttp.data.ts
+++ src/app/outhttp/outhttp.data.ts
@@ -3,7 +3,7 @@ export const OutHTTPComponentConfig: any =
     'name' : 'OutHTTP',
     'table-columns' : [
       { 'title': 'ID', 'name': 'ID' },
-      { 'title': 'Url', 'name': 'Url' },
+      { 'title': 'URL', 'name': 'URL' },
       { 'title': 'Headers', 'name': 'Headers' },
       { 'title': 'AlertTpl', 'name': 'AlertTpl' }
     ]
