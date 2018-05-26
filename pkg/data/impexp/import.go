695d91a3068642a63b0e28c752119fa5c39cc7e6
sbengo <serigna@gmail.com>
2018-05-26 09:30:34 +0200

    Added import and export data functions on backend side

--- pkg/data/impexp/import.go
+++ pkg/data/impexp/import.go
@@ -8,6 +8,9 @@ import (
 	//	"github.com/toni-moreno/resistor/pkg/config"
 	"strconv"
 	"time"
+
+	"github.com/go-macaron/binding"
+	"github.com/toni-moreno/resistor/pkg/config"
 )
 
 func (e *ExportData) ImportCheck() (*ExportData, error) {
@@ -28,8 +31,8 @@ func (e *ExportData) ImportCheck() (*ExportData, error) {
 		}
 		log.Debugf("RAW: %s", raw)
 		switch o.ObjectTypeID {
-		case "xxxxxx":
-		/*	data := config.SnmpDeviceCfg{}
+		case "rangetimecfg":
+			data := config.RangeTimeCfg{}
 			json.Unmarshal(raw, &data)
 			ers := binding.RawValidate(data)
 			if ers.Len() > 0 {
@@ -38,13 +41,13 @@ func (e *ExportData) ImportCheck() (*ExportData, error) {
 				duplicated = append(duplicated, o)
 				break
 			}
-			_, err := dbc.GetSnmpDeviceCfgByID(o.ObjectID)
+			_, err := dbc.GetRangeTimeCfgByID(o.ObjectID)
 			if err == nil {
 				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
 				duplicated = append(duplicated, o)
-			}*/
-		case "yyyyyyyy":
-			/*data := config.InfluxCfg{}
+			}
+		case "ifxservercfg":
+			data := config.IfxServerCfg{}
 			json.Unmarshal(raw, &data)
 			ers := binding.RawValidate(data)
 			if ers.Len() > 0 {
@@ -53,23 +56,86 @@ func (e *ExportData) ImportCheck() (*ExportData, error) {
 				duplicated = append(duplicated, o)
 				break
 			}
-			_, err := dbc.GetInfluxCfgByID(o.ObjectID)
+			_, err := dbc.GetIfxServerCfgByID(o.ObjectID)
 			if err == nil {
 				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
 				duplicated = append(duplicated, o)
-			}*/
-		case "zzzzzzzzz":
-			//
-		case "customfiltercfg":
-			//
-		case "oidconditioncfg":
-			//
-		case "measurementcfg":
-			//
-		case "snmpmetriccfg":
-			//
-		case "measgroupcfg":
-			//
+			}
+		case "kapacitorcfg":
+			data := config.KapacitorCfg{}
+			json.Unmarshal(raw, &data)
+			ers := binding.RawValidate(data)
+			if ers.Len() > 0 {
+				e, _ := json.Marshal(ers)
+				o.Error = string(e)
+				duplicated = append(duplicated, o)
+				break
+			}
+			_, err := dbc.GetKapacitorCfgByID(o.ObjectID)
+			if err == nil {
+				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
+				duplicated = append(duplicated, o)
+			}
+		case "productcfg":
+			data := config.ProductCfg{}
+			json.Unmarshal(raw, &data)
+			ers := binding.RawValidate(data)
+			if ers.Len() > 0 {
+				e, _ := json.Marshal(ers)
+				o.Error = string(e)
+				duplicated = append(duplicated, o)
+				break
+			}
+			_, err := dbc.GetProductCfgByID(o.ObjectID)
+			if err == nil {
+				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
+				duplicated = append(duplicated, o)
+			}
+		case "productgroupcfg":
+			data := config.ProductGroupCfg{}
+			json.Unmarshal(raw, &data)
+			ers := binding.RawValidate(data)
+			if ers.Len() > 0 {
+				e, _ := json.Marshal(ers)
+				o.Error = string(e)
+				duplicated = append(duplicated, o)
+				break
+			}
+			_, err := dbc.GetProductGroupCfgByID(o.ObjectID)
+			if err == nil {
+				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
+				duplicated = append(duplicated, o)
+			}
+		case "outhttpcfg":
+			data := config.OutHTTPCfg{}
+			json.Unmarshal(raw, &data)
+			ers := binding.RawValidate(data)
+			if ers.Len() > 0 {
+				e, _ := json.Marshal(ers)
+				o.Error = string(e)
+				duplicated = append(duplicated, o)
+				break
+			}
+			_, err := dbc.GetOutHTTPCfgByID(o.ObjectID)
+			if err == nil {
+				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
+				duplicated = append(duplicated, o)
+			}
+		case "alertcfg":
+			data := config.AlertIDCfg{}
+			json.Unmarshal(raw, &data)
+			ers := binding.RawValidate(data)
+			if ers.Len() > 0 {
+				e, _ := json.Marshal(ers)
+				o.Error = string(e)
+				duplicated = append(duplicated, o)
+				break
+			}
+			_, err := dbc.GetAlertIDCfgByID(o.ObjectID)
+			if err == nil {
+				o.Error = fmt.Sprintf("Duplicated object %s in the database", o.ObjectID)
+				duplicated = append(duplicated, o)
+			}
 		default:
 			return &ExportData{Info: e.Info, Objects: duplicated}, fmt.Errorf("Unknown type object type %s ", o.ObjectTypeID)
 		}
@@ -105,56 +171,160 @@ func (e *ExportData) Import(overwrite bool, autorename bool) error {
 		}
 		log.Debugf("RAW: %s", raw)
 		switch o.ObjectTypeID {
-		case "xxxxxx":
-			/*
-				log.Debugf("Importing snmpdevicecfg : %+v", o.ObjectCfg)
-				data := config.SnmpDeviceCfg{}
-				json.Unmarshal(raw, &data)
-				var err error
-				_, err = dbc.GetSnmpDeviceCfgByID(o.ObjectID)
-				if err == nil { //value exist already in the database
-					if overwrite == true {
-						_, err2 := dbc.UpdateSnmpDeviceCfg(o.ObjectID, data)
-						if err2 != nil {
-							return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
-						}
-						break
+		case "rangetimecfg":
+			log.Debugf("Importing rangetimecfg : %+v", o.ObjectCfg)
+			data := config.RangeTimeCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetRangeTimeCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateRangeTimeCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
 					}
+					break
 				}
-				if autorename == true {
-					data.ID = data.ID + suffix
-				}
-				_, err = dbc.AddSnmpDeviceCfg(data)
-				if err != nil {
-					return err
-				}*/
-
-		case "yyyyyyy":
-			/*
-				log.Debugf("Importing influxcfg : %+v", o.ObjectCfg)
-				data := config.InfluxCfg{}
-				json.Unmarshal(raw, &data)
-				var err error
-				_, err = dbc.GetInfluxCfgByID(o.ObjectID)
-				if err == nil { //value exist already in the database
-					if overwrite == true {
-						_, err2 := dbc.UpdateInfluxCfg(o.ObjectID, data)
-						if err2 != nil {
-							return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
-						}
-						break
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddRangeTimeCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "ifxservercfg":
+			log.Debugf("Importing ifxservercfg : %+v", o.ObjectCfg)
+			data := config.IfxServerCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetIfxServerCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateIfxServerCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
 					}
+					break
 				}
-				if autorename == true {
-					data.ID = data.ID + suffix
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddIfxServerCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "kapacitorcfg":
+			log.Debugf("Importing kapacitorcfg : %+v", o.ObjectCfg)
+			data := config.KapacitorCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetKapacitorCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateKapacitorCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
+					}
+					break
 				}
-				_, err = dbc.AddInfluxCfg(data)
-				if err != nil {
-					return err
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddKapacitorCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "productcfg":
+			log.Debugf("Importing productcfg : %+v", o.ObjectCfg)
+			data := config.ProductCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetProductCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateProductCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
+					}
+					break
 				}
-			*/
-		case "zzzzzzzzzz":
-			//
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddProductCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "productgroupcfg":
+			log.Debugf("Importing productgroupcfg : %+v", o.ObjectCfg)
+			data := config.ProductGroupCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetProductGroupCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateProductGroupCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
+					}
+					break
+				}
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddProductGroupCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "outhttpcfg":
+			log.Debugf("Importing outhttpcfg : %+v", o.ObjectCfg)
+			data := config.OutHTTPCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetOutHTTPCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateOutHTTPCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
+					}
+					break
+				}
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddOutHTTPCfg(&data)
+			if err != nil {
+				return err
+			}
+		case "alertcfg":
+			log.Debugf("Importing alertcfg : %+v", o.ObjectCfg)
+			data := config.AlertIDCfg{}
+			json.Unmarshal(raw, &data)
+			var err error
+			_, err = dbc.GetAlertIDCfgByID(o.ObjectID)
+			if err == nil { //value exist already in the database
+				if overwrite == true {
+					_, err2 := dbc.UpdateAlertIDCfg(o.ObjectID, &data)
+					if err2 != nil {
+						return fmt.Errorf("Error on overwrite object [%s] %s : %s", o.ObjectTypeID, o.ObjectID, err2)
+					}
+					break
+				}
+			}
+			if autorename == true {
+				data.ID = data.ID + suffix
+			}
+			_, err = dbc.AddAlertIDCfg(&data)
+			if err != nil {
+				return err
+			}
 		default:
 			return fmt.Errorf("Unknown type object type %s ", o.ObjectTypeID)
 		}
