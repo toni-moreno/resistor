254b677291b34c894fdd041793f47753eb8feb30
rsolorzano2410 <rsolorzano2410@gmail.com>
2018-06-06 17:53:46 +0200

    UI Templates: title modified and column TplData not shown in list

--- src/app/template/template.data.ts
+++ src/app/template/template.data.ts
@@ -1,13 +1,13 @@
 export const TemplateComponentConfig: any =
   {
-    'name' : 'Alert Templates',
+    'name' : 'Templates',
     'table-columns' : [
       { 'title': 'ID', 'name': 'ID' },
-      { 'title': 'TrigerType', 'name': 'TrigerType' },
+      { 'title': 'TriggerType', 'name': 'TrigerType' },
       { 'title': 'StatFunc', 'name': 'StatFunc' },
       { 'title': 'CritDirection', 'name': 'CritDirection' },
       { 'title': 'ThresholdType', 'name': 'ThresholdType' },
-      { 'title': 'TplData', 'name': 'TplData' },
+      //{ 'title': 'TplData', 'name': 'TplData' },
     ],
     'slug' : 'templatecfg'
   };
