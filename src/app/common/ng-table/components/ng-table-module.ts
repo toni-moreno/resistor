9d45ada8a60fa3f003d1d906e2ca37db212934d8
rsolorzano2410 <rsolorzano2410@gmail.com>
2018-06-12 10:30:33 +0200

    Last changes for compiling errors

--- src/app/common/ng-table/components/ng-table-module.ts
+++ src/app/common/ng-table/components/ng-table-module.ts
@@ -5,13 +5,14 @@ import { NgTableComponent } from './table/ng-table.component';
 import { NgTableFilteringDirective } from './table/ng-table-filtering.directive';
 import { NgTablePagingDirective } from './table/ng-table-paging.directive';
 import { NgTableSortingDirective } from './table/ng-table-sorting.directive';
+import { keyParserPipe } from './table/ng-table-keypipe';
 import { CustomPipesModule }  from '../../custom-pipe/custom-pipe.module';
 
 //import { ObjectParserPipe as keyParserPipe } from '../../../common/custom_pipe';
 
 @NgModule({
   imports: [CommonModule, TooltipModule, CustomPipesModule],
-  declarations: [NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective],
+  declarations: [NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective, keyParserPipe],
   exports: [NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective]
 })
 export class Ng2TableModule {
