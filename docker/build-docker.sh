5917e97aa84f5b09ea635014649672e542348691
toni-moreno <toni.moreno@gmail.com>
2018-08-04 21:33:28 +0200

    fixed docker image with resinjector entrypoint

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -22,10 +22,12 @@ export COMMIT
 
 cp dist/resistor-${VERSION}-${COMMIT}_${GOOS:-linux}_${GOARCH:-amd64}.tar.gz docker/resistor-last.tar.gz
 cp conf/sample.resistor.toml docker/resistor.toml
+cp conf/sample.resinjector.toml docker/resinjector.toml
 
 cd docker
 
 sudo docker build --label version="${VERSION}" --label commitid="${COMMIT}" -t tonimoreno/resistor:${VERSION} -t tonimoreno/resistor:latest .
 rm resistor-last.tar.gz
 rm resistor.toml
+rm resinjector.toml
 rm /tmp/goenv.tmp
---
+++ docker/start_resinjector.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./bin/resinjector  > ./log/stdout_resinjector.log 2> ./log/stderr_resinjector.log 
